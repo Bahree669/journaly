3a78bb26112debc26cc5a9f331c53de06f269756
Bahree <bahree36@outlook.com>
2022-07-04 05:39:38 +0700

    moved some code to util for modularity reason

---
+++ js/util.js
@@ -0,0 +1,13 @@
+function makeId() {
+    const id = [];
+    const text = "qwertyuiopasdfghjklzxcvbnm_+1234567890-?&QWERTYUIOPASDFGHJKLZXCVBNM".split("");
+    for (let i = 0; i < 12; i++) {
+        id.push(text[random(0, text.length)]);
+    }
+
+    function random(min, max) {
+        return Math.floor(Math.random() * (max - min) + 1);
+    }
+
+    return id.join("");
+}
