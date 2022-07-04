5742c9707261110e9ba4fc3e1eae6712a7462e5a
Bahree <bahree36@outlook.com>
2022-07-04 08:17:12 +0700

    Added a functionality that allow user to mark their daily task to as finished or unfinished

--- js/app.js
+++ js/app.js
@@ -14,6 +14,58 @@ const userArchives = [];
 
 // ======= RENDER EVENTS
 const RENDER_NOTE = "RENDER_NOTE";
+const RENDER_DAILY_TASK = "RENDER_DAILY_TASK";
+
+// ======= DAILY TASKS
+const dailyTaskForm = document.getElementById("myDay_form");
+const dailyTaskContainer = document.getElementById("myDay_tasks");
+
+document.addEventListener("DOMContentLoaded", () => {
+    dailyTaskForm.addEventListener("submit", (e) => {
+        addDailyTask(e);
+    });
+});
+
+function addDailyTask(e) {
+    const { my_day } = journal;
+    e.preventDefault();
+    const taskName = document.getElementById("taskTitle").value;
+    const taskObject = createTaskObject(taskName, null);
+
+    my_day.unshift(taskObject);
+    renderDailyTask();
+}
+
+function createTaskObject(taskName, taskDuedate) {
+    return { id: makeId(), task_title: taskName, due_date: taskDuedate, is_finish: false };
+}
+
+export function finishTask(e, targetId) {
+    const { my_day } = journal;
+    const targetTask = my_day.filter((task) => task.id === targetId)[0];
+    const idxTaskInMyDay = my_day.findIndex((task) => task.id === targetId);
+
+    const taskObject = { ...targetTask, is_finish: targetTask.is_finish ? false : true };
+
+    my_day.splice(idxTaskInMyDay, 1, taskObject);
+    renderDailyTask();
+}
+
+function renderDailyTask() {
+    document.dispatchEvent(new Event(RENDER_DAILY_TASK));
+}
+
+function displayDailyTask() {
+    const { my_day } = journal;
+
+    dailyTaskContainer.innerHTML = "";
+    for (const task of my_day) {
+        const taskCard = createTask("MYDAY", task);
+        dailyTaskContainer.append(taskCard);
+    }
+}
+
+document.addEventListener(RENDER_DAILY_TASK, displayDailyTask);
 
 // ======= DAILY NOTES
 const dailyNote = document.querySelector(".note_text");
@@ -25,10 +77,11 @@ dailyNoteControll.addEventListener("click", addDailyNote);
 
 let noteInput;
 function addDailyNote(e) {
-    journal.daily_note.note = noteInput;
-    journal.daily_note.isEdit = journal.daily_note.isEdit ? false : true;
+    const { daily_note } = journal;
+    daily_note.note = noteInput;
+    daily_note.isEdit = daily_note.isEdit ? false : true;
 
-    console.log(journal.daily_note);
+    console.log("addDailyNote", daily_note);
     renderDailyNote();
 }
 
@@ -37,9 +90,10 @@ function getDailyNote() {
 }
 
 function displayNote() {
-    dailyNote.textContent = journal.daily_note.note;
+    const { daily_note } = journal;
+    dailyNote.textContent = daily_note.note || `🌌 Do not go gentle into that good night. 🌌`;
 
-    if (journal.daily_note.isEdit) {
+    if (daily_note.isEdit) {
         dailyNoteControll.textContent = "Edit Note";
         dailyNote.style.display = "flex";
         dailyNoteInput.style.display = "none";
@@ -47,7 +101,7 @@ function displayNote() {
         dailyNoteControll.textContent = "Add Note";
         dailyNote.style.display = "none";
         dailyNoteInput.style.display = "flex";
-        dailyNoteInput.value = journal.daily_note.note;
+        dailyNoteInput.value = daily_note.note || "";
     }
 }
 
