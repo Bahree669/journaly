a6dc4f65e679789d515adfeb3dbeeb36817d6468
Bahree <bahree36@outlook.com>
2022-07-06 07:56:14 +0700

    Added user task modal element

--- js/htmlElement.js
+++ js/htmlElement.js
@@ -10,7 +10,7 @@ export function createTask(category, task) {
     const input = document.createElement("input");
     input.setAttribute("type", "checkbox");
     input.setAttribute("id", id);
-    input.addEventListener("click", (e) => finishTask(e, id));
+    input.addEventListener("click", () => finishDailyTask(id));
 
     const taskTitle = document.createElement("h6");
     taskTitle.textContent = taskName;
@@ -23,7 +23,13 @@ export function createTask(category, task) {
     }
 
     const taskControll = document.createElement("button");
-    taskControll.textContent = "More";
+    taskControll.innerHTML = `<div class='more_btn'>
+            <div class="more_icon">
+                <div class='dot'></div>
+                <div class='dot'></div>
+                <div class='dot'></div>
+            </div>
+        </div>`;
     taskControll.setAttribute("class", "btn btn_main");
     taskControll.addEventListener("click", () => openModal(modalId));
 
@@ -38,15 +44,15 @@ export function createTask(category, task) {
 
     if (category === "TASK") {
         taskContent.append(taskTitle, taskDueDate);
-        container.append(input, taskContent, taskControll, createTaskModal(category, modalId, taskName, id));
+        container.append(input, taskContent, taskControll, createTaskModal(modalId, id, taskName, dueDate));
     } else {
-        container.append(input, taskTitle, taskControll, createTaskModal(category, modalId, taskName, id));
+        container.append(input, taskTitle, taskControll, createDailyTaskModal(modalId, taskName, id));
     }
 
     return container;
 }
 
-function createTaskModal(category, modalId, taskName, taskId) {
+function createDailyTaskModal(modalId, taskName, taskId) {
     const modal = document.createElement("dialog");
     modal.setAttribute("id", modalId);
 
@@ -83,5 +89,62 @@ function createTaskModal(category, modalId, taskName, taskId) {
     modalControll.append(saveButton, deleteButton, closeButton);
     modalContent.append(dialogTitle, input, modalControll);
     modal.append(modalContent);
+    return modal;
+}
+
+function createTaskModal(modalId, id, taskName, dueDate) {
+    const modal = document.createElement("dialog");
+    modal.setAttribute("id", modalId);
+
+    const dialogContent = document.createElement("div");
+    dialogContent.setAttribute("class", "dialog_content");
+
+    const dialogTitle = document.createElement("h4");
+    dialogTitle.textContent = "Edit Task";
+
+    const form = document.createElement("form");
+    form.setAttribute("id", "userTask_edit");
+    form.setAttribute("class", "myDay_form");
+
+    const inputContainerOne = document.createElement("div");
+    const inputContainerTwo = document.createElement("div");
+
+    const inputTitle = document.createElement("input");
+    inputTitle.setAttribute("type", "text");
+    inputTitle.setAttribute("placeholder", "Go to the store");
+    inputTitle.setAttribute("name", "task title");
+    inputTitle.setAttribute("autocomplete", "off");
+    inputTitle.setAttribute("required", "");
+    inputTitle.setAttribute("id", "userTask_edit_title");
+    inputTitle.setAttribute("value", taskName);
+
+    const inputDate = document.createElement("input");
+    inputDate.setAttribute("type", "date");
+    inputDate.setAttribute("value", dueDate);
+    inputDate.setAttribute("name", "task title");
+    inputDate.setAttribute("id", "userTask_edit_date");
+    inputDate.setAttribute("required", "");
+
+    const btnSubmit = document.createElement("button");
+    btnSubmit.textContent = "Save Edit";
+    btnSubmit.setAttribute("class", "btn btn_main");
+
+    const btnCancel = document.createElement("button");
+    btnCancel.textContent = "Cancel";
+    btnCancel.setAttribute("class", "btn btn_secondary");
+    btnCancel.addEventListener("click", () => closeModal(modalId));
+
+    const btnDelete = document.createElement("button");
+    btnDelete.textContent = "Delete";
+    btnDelete.setAttribute("class", "btn btn_alert");
+
+    inputContainerOne.append(inputTitle);
+    inputContainerTwo.append(inputDate);
+
+    form.append(inputContainerOne, inputContainerTwo, btnSubmit);
+
+    dialogContent.append(dialogTitle, form, btnDelete, btnCancel);
+
+    modal.append(dialogContent);
     return modal;
 }
